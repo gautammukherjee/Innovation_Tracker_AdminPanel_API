e15975c2929ba208513107ac0edb078ba69bf99d
piyushsrivastava80 <psrivastava@bioxcel.com>
2023-03-09 21:22:36 +0530

    api

--- app/Http/Controllers/api/NewsletterController.php
+++ app/Http/Controllers/api/NewsletterController.php
@@ -67,7 +67,27 @@ class NewsletterController extends Controller
 
     //////Get Genes Lists section
     public function getNewsletterFrontLists(Request $request){
-        $sql = "SELECT ns.news_id, ns.user_id, ns.publication_date, ns.title, ns.description, ns.url, ns.webhose_id, ns.textindex_td, ns.created_at, c.name as user_name FROM testing.newss as ns LEFT JOIN users as c ON ns.user_id=c.user_id WHERE ns.deleted=0";
+        // print($request);
+        $publishedDate = '';
+        if($request->from_date!="undefined" && $request->to_date!="undefined"){
+            $publishedDate = " AND publication_date between '". $request->from_date ."' and '".$request->to_date."'";
+        }
+        else if($request->from_date!=""){
+            $publishedDate = " AND publication_date =". $request->from_date;
+        }
+        else if($request->to_date!=""){
+            $publishedDate = " AND publication_date =". $request->to_date;
+        }
+
+        // Ta rels
+        // if($request->ta_id != ""){
+        //     echo "hello1";
+        // }else{
+        //     echo "hello2";
+        // }
+
+        $sql = "SELECT ns.news_id, ns.user_id, ns.publication_date, ns.title, ns.description, ns.url, ns.webhose_id, ns.textindex_td, ns.created_at, c.name as user_name FROM testing.newss as ns LEFT JOIN users as c ON ns.user_id=c.user_id WHERE ns.deleted=0 ".$publishedDate;
+        //echo $sql;
         $result = DB::select(DB::raw($sql));
         return response()->json([
             'newsletterRecords' => $result
